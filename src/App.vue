a7788af0009b0a346d6af476a65ba58bb1d43e07
niyorn <c.niyornram@hotmail.com>
2018-08-28 13:27:27 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,19 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body{
+  height: 100%;
+  scroll-behavior: smooth;
+  font-family: sans-serif;
+}
+  
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
