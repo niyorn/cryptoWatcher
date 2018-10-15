e70d65feef19c93cbbcff1d81920638910d0b110
niyorn <c.niyornram@hotmail.com>
2018-10-15 12:16:13 +0200

    Build production

---
+++ dist/precache-manifest.48ebbf79aa8693f89385c6726558b239.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cd6f0cc0443891a15363",
+    "url": "/js/chunk-vendors.f7b94215.js"
+  },
+  {
+    "revision": "38686113bdfc9d988037",
+    "url": "/js/app.d143a7a3.js"
+  },
+  {
+    "revision": "08da07ce7a09b8fff9669506f8b99bff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38686113bdfc9d988037",
+    "url": "/css/app.8bf5d504.css"
+  }
+];
