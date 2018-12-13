614783f16be35ef257acaf524b58d11b0963286d
niyorn <c.niyornram@hotmail.com>
2018-12-13 07:58:26 +0100

    Change location folder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import store from './store'
+import store from './store/store'
 
 Vue.config.productionTip = false
 
