10ae21682ec6b7f7dd1374d29d390e0d9d15a891
niyorn <c.niyornram@hotmail.com>
2019-01-29 12:15:40 +0100

    Fix back to top

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
     <header id="top">
-        <a href="#top" class="logo">CryptoWatcher</a>
+        <a href="#" class="logo">CryptoWatcher</a>
 
         <button v-show="!search" v-on:click="search = true" class="search-button open">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 512 512"
